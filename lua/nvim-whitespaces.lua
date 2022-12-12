166e78d8b2634cd1b4ad47228304b6634957166a
saccarosium <github.e41mv@aleeas.com>
2022-12-12 21:27:29 +0100

    Fixing structure of setup function

--- lua/nvim-whitespaces.lua
+++ lua/nvim-whitespaces.lua
@@ -2,7 +2,7 @@ local M = {}
 local autocmd = vim.api.nvim_create_autocmd
 local config = {
     on_event = 'BufWritePre',
-    ft_ignore = { 'TelescopePrompt', 'Trouble', 'help' },
+    ft = { '*.lua', '*.vim' },
 }
 
 function M.trim()
@@ -12,10 +12,11 @@ end
 function M.setup(options)
     config = vim.tbl_extend('force', config, options or {})
 
-    vim.api.nvim_create_augroup("nvim-whitespaces", {clear = true})
+    vim.api.nvim_create_augroup("nvim_whitespaces", {clear = true})
+
     autocmd({config["on_event"]}, {
-        group = "sacca",
-        pattern = config["ft_ignore"],
+        group = "nvim_whitespaces",
+        pattern = config["ft"],
         command = "%s/\\s\\+$//e",
     })
 end
