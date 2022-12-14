d074c962a5f7b6b4b5256b9d5509556eac868af5
saccarosium <github.e41mv@aleeas.com>
2022-12-14 18:07:47 +0100

    Adding 'ignore_ft' to setup func

--- lua/nvim-whitespaces.lua
+++ lua/nvim-whitespaces.lua
@@ -2,22 +2,32 @@ local M = {}
 local autocmd = vim.api.nvim_create_autocmd
 local config = {
     on_event = 'BufWritePre',
-    ft = { '*.lua', '*.vim' },
+    ignore_ft = { 'markdown' },
 }
-
-function M.trim()
-    vim.cmd("%s/\\s\\+$//eg")
-end
 
 function M.setup(options)
     config = vim.tbl_extend('force', config, options or {})
 
-    vim.api.nvim_create_augroup("nvim_whitespaces", {clear = true})
+    vim.api.nvim_create_user_command("TrimSpaces", function()
+        local trim = true
+        local buff_ft = vim.bo.filetype
+        for _, i in ipairs(config["ignore_ft"]) do
+            if buff_ft == i then
+                trim = false
+            end
+        end
+        if trim then
+            vim.cmd("%s/\\s\\+$//e")
+        end
+    end,
+    {})
 
-    autocmd({config["on_event"]}, {
+    vim.api.nvim_create_augroup("nvim_whitespaces", { clear = true })
+
+    autocmd({ config["on_event"] }, {
         group = "nvim_whitespaces",
-        pattern = config["ft"],
-        command = "%s/\\s\\+$//e",
+        pattern = "*",
+        command = "TrimSpaces",
     })
 end
 
