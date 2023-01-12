c5c61d3f1a966a432d3be65c694809757f2c30f4
saccarosium <github.e41mv@aleeas.com>
2023-01-12 18:47:17 +0100

    Adding support ignore projects

--- lua/nvim-whitespaces.lua
+++ lua/nvim-whitespaces.lua
@@ -2,28 +2,37 @@ local M = {}
 local autocmd = vim.api.nvim_create_autocmd
 local config = {
     on_event = { 'BufWritePre' },
-    ignore_ft = { 'markdown' },
+    ignore_ft = {},
+    ignore_on_project = {},
 }
+
+local function trim_spaces()
+    local trim = true
+    local cwd = vim.fn.getcwd()
+    for _, dir in ipairs(config["ignore_on_project"]) do
+        if cwd == vim.fn.expand(dir) then
+            trim = false
+            break
+        end
+    end
+    local buff_ft = vim.bo.filetype
+    for _, ft in ipairs(config["ignore_ft"]) do
+        if buff_ft == ft then
+            trim = false
+            break
+        end
+    end
+    if trim then
+        vim.cmd [[%s/\s\+$//eg]]
+    end
+end
 
 function M.setup(options)
     config = vim.tbl_extend('force', config, options or {})
-
-    vim.api.nvim_create_user_command("TrimSpaces", function()
-        local trim = true
-        local buff_ft = vim.bo.filetype
-        for _, i in ipairs(config["ignore_ft"]) do
-            if buff_ft == i then
-                trim = false
-            end
-        end
-        if trim then
-            vim.cmd("%s/\\s\\+$//e")
-        end
-    end,
-    {})
-
+    vim.api.nvim_create_user_command("TrimSpaces", function ()
+        trim_spaces()
+    end, {})
     vim.api.nvim_create_augroup("nvim_whitespaces", { clear = true })
-
     autocmd(config["on_event"], {
         group = "nvim_whitespaces",
         pattern = "*",
